4d99c98d0bb4ac20c11534ccf4c1bf01ef616427
SyntaxTechnologies <instructor@syntaxtechs.com>
2019-03-31 12:12:47 -0400

    Class 15

--- src/com/class14/StringSplit.java
+++ src/com/class14/StringSplit.java
@@ -15,18 +15,6 @@ public class StringSplit {
 			System.out.println(array[i]);
 		}
 		//("\\?")//[!?]
-		int num=37;
-		boolean isPrime=true;
-		for (int i=2; i<num/2; i++) {
-			if (num%i==0) {
-				isPrime=false;
-			}
-		}
-		if (isPrime) {
-			System.out.println(num+" is a prime number");
-		}else {
-			System.out.println(num+" is NOT a prime number");
-		}
 
 	}
 }
